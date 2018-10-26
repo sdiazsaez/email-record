2dc66d79760a7d17cebd10bbb16b94f7462240bc
simon <sdiaz.sz@gmail.com>
2018-10-26 05:34:04 -0300

    add support for name_type into email-record config

--- src/Models/EmailRequest.php
+++ src/Models/EmailRequest.php
@@ -11,15 +11,18 @@ class EmailRequest extends Model {
 
     use SoftDeletes, RoutingModel;
 
-    protected $table    = 'email_requests';
-    protected $dates    = ['deleted_at'];
+    protected $table = 'email_requests';
+    protected $dates = ['deleted_at'];
     protected $fillable = [
         'content_id',
         'email_type',
         'sent_email_id',
     ];
-    protected $with     = ['sentEmail'];
-    protected $appends  = ['emailTypeClass'];
+    protected $with = ['sentEmail'];
+    protected $appends = [
+        'emailTypeClass',
+        'emailTypeName',
+    ];
 
     public function __construct(array $attributes = []) {
         parent::__construct($attributes);
@@ -32,8 +35,11 @@ class EmailRequest extends Model {
     }
 
     public function getEmailTypeClassAttribute() {
-        $type = $this->attributes['email_type'];
-        return config('email-record.email_types.' . $type);
+        return config('email-record.email_types.' . $this->attributes['email_type'] . '.type_class');
+    }
+
+    public function getEmailTypeNameAttribute() {
+        return config('email-record.email_types.' . $this->attributes['email_type'] . '.type_name');
     }
 
     public function scopeNotSent($query) {
